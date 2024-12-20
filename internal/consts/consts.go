c86ecfa0be8f8446d084303167216e12a6eb4604
Tianxiang <22270129@hdu.edu.cn>
2024-12-20 15:00:20 +0800

    增加订单状态映射常量

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,1 +1,12 @@
 package consts
+
+type OrderStatus int
+
+const (
+	OrderStatusPending    OrderStatus = 0 // 订单待处理
+	OrderStatusProcessing OrderStatus = 1 // 订单处理中
+	OrderStatusShipped    OrderStatus = 2 // 订单已发货
+	OrderStatusDelivered  OrderStatus = 3 // 订单已送达
+	OrderStatusCancelled  OrderStatus = 4 // 订单已取消
+	OrderStatusReturned   OrderStatus = 5 // 订单已退货
+)
